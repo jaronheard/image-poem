bdffec6855483821d8b1622b00b4c0165db01595
Jaron Heard <jaronheard@gmail.com>
2022-08-05 12:07:51 -0700

    add lint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { ChangeEvent, useEffect, useState } from "react";
 
 const Home: NextPage = () => {
