f0185e86c80b5819e83bd4ca90ee852c71ca5546
Jaron Heard <jaronheard@gmail.com>
2022-08-05 17:32:52 -0700

    add full screen app capability

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ const Home: NextPage = () => {
       <Head>
         <title>image poem</title>
         <meta name="description" content="a poetic image generator" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎨</text></svg>"
