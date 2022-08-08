418014bb1eb965708f49e83800de0ff368632fe0
Jaron Heard <jaronheard@gmail.com>
2022-08-07 17:42:00 -0700

    use-api-flash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,6 +108,14 @@ const DownloadScreenShot = ({ url }: DownloadScreenShotProps) => {
   );
 };
 
+const DownloadAPIFlashScreenshot = ({ url }: DownloadScreenShotProps) => (
+  <a
+    href={`https://api.apiflash.com/v1/urltoimage?access_key=3cdc3508085a4cf9a30283bb17f50a13&url=${url}&format=png&width=900&height=1600&fresh=true&wait_until=network_idle&delay=3`}
+  >
+    <DownloadIcon />
+  </a>
+);
+
 const Home: NextPage = () => {
   return (
     <>
@@ -252,7 +260,7 @@ const UploadImages = () => {
                 className={`text-[${textColor}] file:text-[${textColor}] block text-sm file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:py-2 file:px-4 file:text-sm file:font-semibold hover:file:bg-violet-100`}
               />
               {image !== "" && (
-                <DownloadScreenShot
+                <DownloadAPIFlashScreenshot
                   url={`https://image-poem.vercel.app${router.asPath}`}
                 />
               )}
